09d8b31d5e633e0efe31d28de2fdf52b2dae7d98
yukisako <yukisakoatoz@gmail.com>
2015-12-15 17:34:45 +0900

    [fix]ディレクトリ構成変えたから一括eps変換コマンドも変更

--- mytex.sh
+++ mytex.sh
@@ -7,8 +7,11 @@
 ################################
 if [ "$1" = '-i' ]
 then
+  cd ./img/png/
+  pwd
   for infile in *.png; do
-  convert img/png/$infile img/eps/${infile%.*}.eps ;
+  echo $infile
+  convert $infile ../eps/${infile%.*}.eps ;
   done
 fi
 
@@ -16,5 +19,7 @@ if [ "$1" = 'init' ]
 then
   cp -a /Users/yukisako/tools/tex-example ./
   mv tex-example $2
-  cd $2
+  echo ${2}/
+  cd ${2}/
+
 fi
